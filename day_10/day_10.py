89bc48b300bedd372fbb40bc2c5b16b5fe726142
psidza <penzijoner@gmail.com>
2020-12-17 15:52:51 +0100

    added day2 part2

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,14 +1,25 @@
 # Part 1
 with open("input.txt", "r") as data_input:
-    adaptors = sorted([int(adaptor.rstrip()) for adaptor in data_input])
+    adapters = sorted([int(adaptor.rstrip()) for adaptor in data_input])
 
-# preppending 0 and appending 3 to the list
-adaptors.insert(0, 0)
-adaptors.append(adaptors[-1] + 3)
+# preppending 0 as outlet joltage and appending new element that's sum of highest rated adaptor and 3 jolts
+adapters.insert(0, 0)
+adapters.append(adapters[-1] + 3)
 
-# generating list of difference in voltage between adapters
-diff = [adaptors[i] - adaptors[i - 1] for i in range(1, len(adaptors))]
+
+# generating list of difference in joltage between adapters
+diff = [adapters[i] - adapters[i - 1] for i in range(1, len(adapters))]
 
 print(f"Part 1 | Number of 1-jolt differences times number of 3-jolt differences: {diff.count(1) * diff.count(3)}")
 
 # Part 2
+# generating list of ways to each adaptor in the list by calculating acceptable difference in jolts
+# number of acceptable joltages between 2 adaptors
+number_of_ways = [1]
+for i in range(1, len(adapters)):
+    number_of_ways.append(0)
+    for j in [1, 2, 3]:
+        if i >= j and (adapters[i] - adapters[i - j] <= 3):
+            number_of_ways[i] += number_of_ways[i - j]
+
+print(f"Part 2 | Number of distinct ways: {number_of_ways[-1]}")
